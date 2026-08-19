ae862b9e8789a6cde929b9c484a4a1fb30104c55
adaybekovt-boop <adaybekovt@bk.ru>
2026-08-19 15:25:00 +0500

    Union cache lifecycle with hopper cleanup

--- src/main/java/dev/ultima/Ultima.java
+++ src/main/java/dev/ultima/Ultima.java
@@ -4,7 +4,10 @@ import dev.ultima.cache.RegistryCacheLifecycle;
 import dev.ultima.command.UltimaCommands;
 import dev.ultima.config.UltimaConfig;
 import dev.ultima.server.metrics.ServerMetrics;
+import dev.ultima.sleeping.BlockEntitySleepRuntime;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLevelEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,5 +23,7 @@ public final class Ultima implements ModInitializer {
         ServerMetrics.setEnabled(UltimaConfig.get().isEnabled(ServerMetrics.MODULE_KEY));
         RegistryCacheLifecycle.register();
         UltimaCommands.register();
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> BlockEntitySleepRuntime.clearAll());
+        ServerLevelEvents.UNLOAD.register((server, level) -> BlockEntitySleepRuntime.clearLevel(level));
     }
 }
