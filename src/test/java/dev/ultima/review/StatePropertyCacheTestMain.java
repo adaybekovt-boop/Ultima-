a2d2c4ea1ecfbb4e2bf1583a87f7cd18115fa8cb
adaybekovt-boop <adaybekovt@bk.ru>
2026-08-19 14:26:39 +0500

    Add standalone state property regression launcher

---
+++ src/test/java/dev/ultima/review/StatePropertyCacheTestMain.java
@@ -0,0 +1,12 @@
+package dev.ultima.review;
+
+import dev.ultima.cache.StatePropertyCacheEquivalenceTest;
+
+public final class StatePropertyCacheTestMain {
+    private StatePropertyCacheTestMain() {
+    }
+
+    public static void main(final String[] args) {
+        StatePropertyCacheEquivalenceTest.run();
+    }
+}
